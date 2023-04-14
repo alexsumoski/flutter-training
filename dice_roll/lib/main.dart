88d9d80a29a641fe2e73a0c9c80b67cf87bfdf1f
alexsumoski <alexsumoski@gmail.com>
2023-04-13 23:05:02 -0400

    dart classes

--- dice_roll/lib/main.dart
+++ dice_roll/lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 void main() {
-  runApp( MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              colors: [ Color.fromARGB(255, 237, 99, 24), Color.fromARGB(255, 237, 162, 24) ]
-            ),
-        ),
-        child: const Center(
-          child: Text("Center"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    )
+  );
+}
+
+class GradientContainer extends StatefulWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [ Color.fromARGB(255, 223, 71, 29), Color.fromARGB(255, 237, 162, 24) ]
+          ),
+      ),
+      child: const Center(
+        child: Text(
+          "Center",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.w700
+          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
